f6667f936c8c798290428ab1a129ed80a41181f2
Felipe Baytelman <felipe.baytelman@gmail.com>
2016-06-28 00:07:40 -0700

    Move "My Apps", add "MacDown"

--- init/install_all.sh
+++ init/install_all.sh
@@ -5,12 +5,21 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew update;
 brew install caskroom/cask/brew-cask 2> /dev/null
 
+# My apps
+brew cask install atom;
+brew cask install macdown;
+brew cask install cyberduck;
+brew cask install sourcetree;
+brew cask install google-chrome;
+brew cask install sequel-pro;
+brew cask install dropbox;
+brew cask install pycharm;
+
 # Python stuff
 brew install python;
 brew install pyenv;
 pyenv install 3.4.3;
 pip install --upgrade virtualenv;
-brew cask install pycharm;
 
 # node
 brew install node;
@@ -23,11 +32,3 @@ brew cask install virtualbox;
 brew cask install vagrant;
 brew install docker-machine;
 brew install docker-compose;
-
-# My apps
-brew cask install atom;
-brew cask install cyberduck;
-brew cask install sourcetree;
-brew cask install google-chrome;
-brew cask install sequel-pro;
-brew cask install dropbox;
