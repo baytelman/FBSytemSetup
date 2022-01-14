f59361424b3263bf08fe3a0dc92f4034e26746ab
Felipe Baytelman <felipe.baytelman@gmail.com>
2022-01-14 13:13:22 -0500

    Reorder, add asimov

--- init/init-all.sh
+++ init/init-all.sh
@@ -9,6 +9,11 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 brew update;
 brew install caskroom/cask/brew-cask 2> /dev/null
 
+# System utilities
+## Asimov – Prevent development caches to be included in backups
+brew install asimov
+sudo brew services start asimov
+
 # My apps
 brew install --cask visual-studio-code;
 brew install --cask sourcetree;
@@ -16,17 +21,20 @@ brew install --cask google-chrome;
 brew install --cask robo-3t;
 brew install --cask figma;
 
-# node
-brew install node@12;
-brew install npm;
-# Add to sh
-echo 'export PATH="/opt/homebrew/opt/node@12/bin:$PATH"' >> ~/.zshrc
-export LDFLAGS="-L/opt/homebrew/opt/node@12/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/node@12/include"
-
 # comm
 brew install --cask whatsapp
 brew install --cask slack
+
+# Fun
+brew install --cask spotify
+
+# node
+brew install node@14;
+brew install npm;
+# Add to sh
+echo 'export PATH="/opt/homebrew/opt/node@14/bin:$PATH"' >> ~/.zshrc
+export LDFLAGS="-L/opt/homebrew/opt/node@14/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/node@14/include"
 
 # nvm
 brew install nvm;
@@ -36,5 +44,3 @@ echo "source $(brew --prefix nvm)/nvm.sh" >> ~/.profile
 npm install -g yarn;
 
 
-# Fun
-brew install --cask spotify
