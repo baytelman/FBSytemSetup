404b53f19cefb15d0776e03efa32221d681b7f07
Felipe Baytelman <felipe.baytelman@gmail.com>
2024-07-17 23:21:58 -0400

    Update current.sh
    re-install

--- current.sh
+++ current.sh
@@ -10,18 +10,18 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # System utilities
 ## Asimov – Prevent development caches to be included in backups
-brew install asimov
+brew reinstall asimov
 sudo brew services start asimov
-brew install --cask monitorcontrol
+brew reinstall --cask monitorcontrol
 
 # My apps
-brew install visual-studio-code;
-brew install sourcetree;
-brew install google-chrome;
-brew install figma;
-brew install --cask dropbox;
-brew install docker
-brew install --cask docker;
+brew reinstall visual-studio-code;
+brew reinstall sourcetree;
+brew reinstall google-chrome;
+brew reinstall figma;
+brew reinstall --cask dropbox;
+brew reinstall docker
+brew reinstall --cask docker;
 
 # Database clients
 brew install pgadmin4
@@ -29,31 +29,31 @@ brew install dbeaver-community
 brew install robo-3t;
 
 # comm
-brew install whatsapp
-brew install slack
-brew install --cask discord;
+brew reinstall whatsapp
+brew reinstall slack
+brew reinstall --cask discord;
 
 # Fun
-brew install spotify
+brew reinstall spotify
 
 # node
-brew install node@16;
-brew install npm;
+brew reinstall node@16;
+brew reinstall npm;
 # Add to sh
 echo 'export PATH="/opt/homebrew/opt/node@14/bin:$PATH"' >> ~/.zshrc
 export LDFLAGS="-L/opt/homebrew/opt/node@14/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/node@14/include"
 
 # nvm
-brew install nvm;
+brew reinstall nvm;
 source $(brew --prefix nvm)/nvm.sh;
 echo "source $(brew --prefix nvm)/nvm.sh" >> ~/.zprofile
 
-npm install -g yarn;
+npm reinstall -g yarn;
 
 # gif
-brew install ffmpeg;
-brew install gifsicle;
+brew reinstall ffmpeg;
+brew reinstall gifsicle;
 
 echo 'gif() {
     ffmpeg -i $1 $1.gif -filter_complex "[0:v] mpdecimate,fps=3,split [a][b];[a] palettegen=max_colors=48 [p];[b][p] paletteuse=dither=none " && gifsicle -O3 $1.gif -o $1.gif
